a4e0211c2104ca67ca3647c854f3d618b1c89e25
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2025-09-27 08:41:34 +0300

    lab1 task4
    Записать видео из файла в другой файл.

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -20,3 +20,23 @@ def task3():
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == 27:
             break
+
+def task4():
+    video = cv2.VideoCapture('../res/vids/video1.mp4', cv2.CAP_ANY)
+    ret, frame = video.read()
+    w = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = video.get(cv2.CAP_PROP_FPS)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    video_writer = cv2.VideoWriter("../res/vids/video1.avi", fourcc, fps, (w, h))
+    while (True):
+        ret, frame = video.read()
+        if not ret:
+            break
+
+        cv2.imshow('Driving', frame)
+        video_writer.write(frame)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    video.release()
+    cv2.destroyAllWindows()
