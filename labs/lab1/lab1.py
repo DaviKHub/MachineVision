69b50176557a8cfe48bbe8e4a101823e3484152e
DaviKHub <175750928+DaviKHub@users.noreply.github.com>
2025-09-27 08:39:42 +0300

    lab1 task2
    Вывести на экран изображение. Протестировать три возможных расширения, три различных флага для создания окна и три различных флага для чтения изображения.

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -1,2 +1,12 @@
 # task1
 import cv2
+
+def task2():
+    img_1 = cv2.imread("../../res/pics/1.jpg", flags=0)
+    cv2.namedWindow("image_1", cv2.WINDOW_NORMAL)
+    cv2.imshow('image_1', img_1)
+    img_2 = cv2.imread("../../res/pics/2.png", flags=-1)
+    cv2.namedWindow("image_2", cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('image_2', img_2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
