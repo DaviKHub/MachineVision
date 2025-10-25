da024793ab1a207a54b3abaaad2637844d985351
DaviKHub <klichenkod2005@gmail.com>
2025-10-25 10:40:53 +0300

    lab2 task2

---
+++ labs/lab2/filter_image.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+def red_mask(hsv_img):
+
+    lower_red1 = np.array([0, 120, 70])
+    upper_red1 = np.array([10, 255, 255])
+    
+    lower_red2 = np.array([170, 120, 70])
+    upper_red2 = np.array([180, 255, 255])
+
+    mask1 = cv2.inRange(hsv_img, lower_red1, upper_red1)
+    mask2 = cv2.inRange(hsv_img, lower_red2, upper_red2)
+
+    return mask1 + mask2
